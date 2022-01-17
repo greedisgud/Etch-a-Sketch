eedb5b8139bace7d1225b130551bb0d326cbf226
Evan Hayes <hayesevan7@gmail.com>
2022-01-17 13:46:01 -0500

    added rgb button

--- app.js
+++ app.js
@@ -30,3 +30,20 @@ function blackColor(){
 
 blackColor();
 
+
+function rgbColor(){
+    const boxs = document.querySelectorAll(".box");
+    rgbBtn.textContent = "Rainbow";
+    rgbBtn.addEventListener("click", () => {
+        boxs.forEach(box => box.addEventListener("mouseover", () => {
+            let red = Math.floor(Math.random()*255);
+            let green = Math.floor(Math.random()*255);
+            let blue = Math.floor(Math.random()*255);
+            box.style.background = `rgb(${red}, ${green}, ${blue})`;
+        }))
+    })
+    btnContainer.appendChild(rgbBtn).classList.add("button");
+}
+
+rgbColor();
+
