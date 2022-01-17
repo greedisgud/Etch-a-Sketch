48bf5f2f1558c0226d800320e4240c15fe38cdd1
Evan Hayes <hayesevan7@gmail.com>
2022-01-17 12:27:59 -0500

    added const buttons

---
+++ app.js
@@ -0,0 +1,6 @@
+let containter = document.querySelector(".container");
+let blackBtn = document.createElement("button");
+let rgbBtn = document.createElement("button");
+let gridSize = document.createElement("button");
+let resetBtn = document.createElement("button");
+
