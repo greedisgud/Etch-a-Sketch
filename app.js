0a0d1293e3070d7bbf6ef0e99452c68e91ee88e4
Evan Hayes <hayesevan7@gmail.com>
2022-01-17 12:37:32 -0500

    created grid

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ let rgbBtn = document.createElement("button");
 let gridSize = document.createElement("button");
 let resetBtn = document.createElement("button");
 
+function createGrid(cols, rows){
+    for(let i = 0; i < (cols*rows); i ++){
+        const div = document.createElement("div");
+        containter.style.gridTemplateColumns = `repeat${cols}, ifr`;
+        containter.style.gridTemplateRows = `repeat${rows}, ifr`;
+        containter.appendChild(div).classList.add("box");
+    }
+}
+
+createGrid(16,16);
